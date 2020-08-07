ff7365f91582222ee1509f275bc2969de2661ac8
JVMedeiros <jv.medeiros.gallina@gmail.com>
2020-08-07 12:26:52 -0300

    test

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,13 @@
-import express from 'express';
+import express, { response } from 'express';
+
+const app = express();
+
+app.get('/users', (request, response) => {
+    return response.json('hello world');
+})
+
+
+app.listen(3333);
+
+
+//Acesso em localhost:3333

3a28520dc9911a3e976c153c2e330750a762f630
JVMedeiros <jv.medeiros.gallina@gmail.com>
2020-08-11 11:51:01 -0300

    description props implemented

--- web/src/components/PageHeader/index.tsx
+++ web/src/components/PageHeader/index.tsx
@@ -28,6 +28,7 @@ const PageHeader: React.FC<PageHeaderProps> = (props) => {
 
             <div className="header-content">
                 <strong>{props.title}</strong>
+                {props.description && <p>{props.description}</p>}
             </div>
             {props.children}
     </header>

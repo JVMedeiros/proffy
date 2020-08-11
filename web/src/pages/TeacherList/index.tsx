1a7e986aa113fafb84f299ad10c37346639bf859
JVMedeiros <jv.medeiros.gallina@gmail.com>
2020-08-11 11:51:42 -0300

    cleanup

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -13,10 +13,7 @@ function TeacherList() {
         <div id="page-teacher-list" className="container">
             <PageHeader title="Estes são os professores disponíveis">
                 <form id="search-teachers">
-                    <div className="input-block">
-                        <label htmlFor="subject">Matéria</label>
-                        <input type="text" id="subject"/>
-                    </div>
+
 
                     <div className="input-block">
                         <label htmlFor="week-day">Dia da semana</label>

aa757bbbf82053b7a3ac8cde6af1b1c244b541d0
JVMedeiros <jv.medeiros.gallina@gmail.com>
2020-08-11 11:51:28 -0300

    teacher form update

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -11,8 +11,29 @@ function TeacherForm() {
         <div id="page-teacher-form" className="container">
             <PageHeader 
                 title="Que incrível que você quer dar aulas."
-                description="O primeiro passo, é se cadastrar."
+                description="O primeiro passo é preencher este formulário de inscrição."
             />
+
+            <main>
+                <fieldset>
+                    <legend>Seus dados</legend>
+
+                    <div className="input-block">
+                        <label htmlFor="name">Nome completo</label>
+                        <input type="text" id="name"/>
+                    </div>
+
+                    <div className="input-block">
+                        <label htmlFor="avatar">Avatar</label>
+                        <input type="text" id="avatar"/>
+                    </div>
+
+                    <div className="input-block">
+                        <label htmlFor="whatsapp">Whatsapp</label>
+                        <input type="text" id="whatsapp"/>
+                    </div>
+                </fieldset>
+            </main>
         </div>
     )
 }
